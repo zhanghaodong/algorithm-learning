d06311f64a23a82bdf8b791701d2cc70a4358fb9
HaoDongZhang <haodong.zhang@lexisnexis.com>
2020-08-31 17:18:41 +0800

    Select sort

--- algorithm/Sort.go
+++ algorithm/Sort.go
@@ -39,13 +39,33 @@ func insertSort(nums []int)  {
 	}
 }
 
-//todo
 func selectSort(nums []int)  {
-
+	if len(nums) <=1 {
+		return
+	}
+	for i:=0;i<len(nums)-1;i++ {
+		minIndex := len(nums)-1
+		minValue := nums[minIndex]
+		for k:=i;k<len(nums);k++ {
+			if minValue > nums[k] {
+				minValue = nums[k]
+				minIndex = k
+			}
+		}
+		temp := nums[minIndex]
+		nums[minIndex] = nums[i]
+		nums[i] = temp
+	}
 }
 
 func main() {
 	nums := []int{4,5,6,3,2,1}
 	insertSort(nums)
-	fmt.Print(nums)
+	fmt.Println(nums)
+	nums = []int{4,5,6,11,7,8,3,2,1}
+	bubbleSort(nums)
+	fmt.Println(nums)
+	nums = []int{1,3,7,3,9,2,5,7}
+	selectSort(nums)
+	fmt.Println(nums)
 }
