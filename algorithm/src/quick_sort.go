a52e9ca15705c6410a3587bc2fb3a19c3135b382
HaoDongZhang <haodong.zhang@lexisnexis.com>
2020-09-01 16:49:43 +0800

    Quick sort implementation

--- algorithm/src/quick_sort.go
+++ algorithm/src/quick_sort.go
@@ -1,7 +1,34 @@
 package src
 
-
-//todo
 func quickSort(nums []int)  {
+	quickSortHelper(nums,0, len(nums)-1)
+}
 
+func quickSortHelper(nums []int, start, end int)  {
+	if start >= end {
+		return
+	}
+	position := partition(nums, start, end)
+	quickSortHelper(nums, start, position-1)
+	quickSortHelper(nums, position+1, end)
+}
+
+/*
+	6,11,3,9,8
+	6 < 8 swap
+	11 > 8 no swap
+	3 < 8 swap
+	9 > 8 no swap
+*/
+func partition(nums []int, start, end int)  int{
+	pivot := nums[end]
+	left := start
+	for i:=start; i< end; i++ {
+		if nums[i] < pivot {
+			nums[i], nums[left] = nums[left], nums[i]
+			left++
+		}
+	}
+	nums[end], nums[left] = nums[left], nums[end]
+	return left
 }
--- algorithm/src/sort_test.go
+++ algorithm/src/sort_test.go
@@ -64,5 +64,15 @@ func TestMergeSort(t *testing.T)  {
 }
 
 func TestQuickSort(t *testing.T)  {
+	numberTests := []numberTest{
+		{[]int{5,2,5,7,1,0,8}, []int{0,1,2,5,5,7,8}},
+		{[]int{6,5,4,1,2,3}, []int{1,2,3,4,5,6}},
+	}
 
+	for _, expected := range numberTests {
+		quickSort(expected.in)
+		if !reflect.DeepEqual(expected.in, expected.out) {
+			t.Errorf("Unit test execute failed, output %v", expected.in)
+		}
+	}
 }
